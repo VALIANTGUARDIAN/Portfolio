b9dc957f5e8501aeb2856789073a03f6431b2bff
VALIANTGUARDIAN <singhdigvijay703@gmail.com>
2022-09-16 21:14:29 +0530

    header completed

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {FiLinkedin} from 'react-icons/fi'
+import {DiGithubAlt} from 'react-icons/di'
+import {AiOutlineInstagram} from 'react-icons/ai'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+      <a href="https://linkedin.com" target="_blank"><FiLinkedin /></a>
+      <a href="https://github.com" target="_blank"> <DiGithubAlt /></a>
+      <a href="https://instagram.com" target="_blank"><AiOutlineInstagram /></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
