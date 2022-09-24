f0e8b1d561f8e705bc335509d427552a7fe9e7d3
VALIANTGUARDIAN <singhdigvijay703@gmail.com>
2022-09-24 22:08:44 +0530

    contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,27 @@ import './contact.css';
 import { MdOutlineEmail } from 'react-icons/md'
 import { RiMessengerLine } from 'react-icons/ri'
 import { BsWhatsapp } from 'react-icons/bs'
+import { useRef } from 'react'
+import emailjs from 'emailjs-com'
+
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    
+
+    emailjs.sendForm('service_stpvybf', 'template_d66tcs2', form.current, 'Ym38qOLhbk6dIfaS4')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -30,7 +49,7 @@ const Contact = () => {
         </div>
 
         {/* form */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Enter Full Name' required />
           <input type="email" name='email' placeholder='Email' required />
           <textarea name="message" rows="10" placeholder='Message' required></textarea>
