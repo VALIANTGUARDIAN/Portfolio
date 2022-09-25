5f5574a23c978c59a4c16381479884ec8b248274
VALIANTGUARDIAN <singhdigvijay703@gmail.com>
2022-09-26 00:42:53 +0530

    github

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,7 +7,7 @@ const HeaderSocials = () => {
   return (
     <div className='header__socials'>
       <a href="hhttps://www.linkedin.com/in/digvijaysinghvg/" target="_blank"><FiLinkedin /></a>
-      <a href="https://github.com" target="_blank"> <DiGithubAlt /></a>
+      <a href="https://github.com/VALIANTGUARDIAN" target="_blank"> <DiGithubAlt /></a>
       <a href="https://instagram.com" target="_blank"><AiOutlineInstagram /></a>
     </div>
   )
